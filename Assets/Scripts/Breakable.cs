74adc61f2e73f8685e00b2aa49c540d17fcc1a8d
Chayats <chayat@hotmail.com>
2017-11-18 15:07:46 +0000

    Added WobbleTree

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+
+    public float breakforce = 5f;
+    public bool isBreakable = true;
+    // Use this for initialization
+    void Start()
+    {
+        breakforce = 5f;
+        isBreakable = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.relativeVelocity.magnitude);
+
+        if ((collision.relativeVelocity.magnitude >= breakforce)&&isBreakable)
+        {
+            Debug.Log("break this");
+            Destroy(gameObject);
+        }
+            
+    }
+}
