2e036dd5c5f21faf8a6ee881a190754283a9544f
Chayats <chayat@hotmail.com>
2017-11-18 13:17:29 +0000

    Added Grav Control and Base Assets

---
+++ Assets/CamController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour {
+
+    public Quaternion camrot;
+
+	// Use this for initialization
+	void Start () {
+        camrot = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.rotation = camrot;
+
+	}
+}
---
+++ Assets/GravityController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour {
+
+
+    public float xpos;
+    public float ypos;
+    public Vector3 mousepos;
+
+    public float gravpower =10f;
+
+	// Use this for initialization
+	void Start () {
+        gravpower = 10f;
+        Physics.gravity = new Vector3(0, 10.0F, 0);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        mousepos = Input.mousePosition;
+        mousepos.x = mousepos.x / Screen.width;
+        mousepos.y = mousepos.y / Screen.height;
+
+        mousepos = mousepos * 2;
+
+        mousepos.x = mousepos.x - 1;
+        mousepos.y = mousepos.y - 1;
+
+        mousepos = mousepos * gravpower;
+        Physics.gravity = mousepos;
+
+    }
+
+    
+
+
+
+}
