9e324999a2c87bf15dc49487d03b4dba2416155d
Chayats <chayat@hotmail.com>
2017-11-18 13:30:37 +0000

    Grav control Update

---
+++ Assets/Breakable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+
+    public float breakforce = 5f;
+
+	// Use this for initialization
+	void Start () {
+        breakforce = 5f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
